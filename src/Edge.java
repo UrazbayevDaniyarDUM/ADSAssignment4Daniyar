31e98da4d3f147aa04208e97d2916d6320f0a739
daniar <254724@astanait.edu.kz>
2026-05-21 15:59:57 +0500

    edge changed

--- src/Edge.java
+++ src/Edge.java
@@ -1,21 +1,21 @@
 public class Edge {
-    private Vertex source;
-    private Vertex destination;
+    private int destination;
+    private int weight;
 
-    public Edge(Vertex source, Vertex destination) {
-        this.source = source;
+    public Edge(int destination, int weight) {
         this.destination = destination;
+        this.weight = weight;
     }
 
-    public Vertex getSource() {
-        return source;
-    }
-
-    public Vertex getDestination() {
+    public int getDestination() {
         return destination;
     }
 
+    public int getWeight() {
+        return weight;
+    }
+
     public String toString() {
-        return source + " -> " + destination;
+        return destination + "(" + weight + ")";
     }
 }
